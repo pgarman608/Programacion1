8365b39f7a3563bd2fd564bbed34bc98beefde37
Pablo_García <cnsg2198@gmail.com>
2022-01-19 18:00:19 +0100

    segundos archivos

---
+++ 2Trimestre/relacion1/ejercicio1/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author serrperry
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
